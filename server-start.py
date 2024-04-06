2352bf3259db1b3d89fb8a83d2cf1c7a85f1049c
mjlue-uc <131723486+mjlue-uc@users.noreply.github.com>
2024-04-06 04:40:43 +0000

    Added API stuff

--- server-start.py
+++ server-start.py
@@ -12,9 +12,17 @@ DIRECTORY = "/path/to/your/directory"
 import os
 os.chdir(DIRECTORY)
 
+
 # Create a TCP server
 with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
     print("Server started at port", PORT)
     
     # Start serving requests indefinitely
     httpd.serve_forever()
+
+@app.route('\users')
+def get_faciltiies():
+    api_key = request.args.get('api_key')
+    headers = {'Authorization' : 'Bearer ' + api_key}
+    response = requests.get(url, headers=headers)
+    print(response)
