4ca6c172529f35de441ac398c16290946d960498
mjlue-uc <mjlue@ucsc.edu>
2024-04-06 12:20:01 -0700

    Added main web server functionality

--- server-start.py
+++ server-start.py
@@ -1,28 +1,42 @@
 import http.server
 import socketserver
 
+from flask import Flask, render_template, request
+import requests
+
 # Define the port you want the server to listen on
 PORT = 8000
 
 # Define the directory path you want to serve files from
-DIRECTORY = "/path/to/your/directory"
+DIRECTORY = ""
 
 # Change the current working directory to the specified directory
 # This is necessary because SimpleHTTPRequestHandler serves files relative to the current directory
 import os
-os.chdir(DIRECTORY)
+#os.chdir(DIRECTORY)
 
 
 # Create a TCP server
-with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
-    print("Server started at port", PORT)
+#with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
+    #print("Server started at port", PORT)
     
     # Start serving requests indefinitely
-    httpd.serve_forever()
+    #httpd.serve_forever()
 
-@app.route('\users')
+app = Flask("parkSF")
+
+@app.route(r'/users')
 def get_faciltiies():
+    url = "https://osp.cit.cc.api.here.com/parking/segments?bbox=41.389405513925354,2.127549994463742,41.38042236108416,2.139522979169079&geometryType=tpegOpenLR&geometryType=segmentAnchor"
     api_key = request.args.get('api_key')
     headers = {'Authorization' : 'Bearer ' + api_key}
     response = requests.get(url, headers=headers)
-    print(response)
+    print(response.status_code)
+    #print(response)
+    return str(response.status_code)
+
+@app.route(r'/main')
+def get_main():
+    return render_template("main.html")
+
+app.run(debug=True)
