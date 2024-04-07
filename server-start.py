6a1be5e0f965001b16ed878ee08b58db94f473f4
mjlue-uc <mjlue@ucsc.edu>
2024-04-07 03:28:37 -0700

    Fixed login system

--- server-start.py
+++ server-start.py
@@ -3,8 +3,9 @@ import socketserver
 
 from flask import Flask, render_template, request, redirect, url_for
 import requests
-from database_operation import add_users_to_db
-from database_operation import verify_user_credentials
+#from database_operation import add_users_to_db
+#from database_operation import verify_user_credentials
+import database_operation
 
 # Define the port you want the server to listen on
 PORT = 8000
@@ -50,7 +51,7 @@ def login():
         password = request.form['password']
         
         # Query the database to verify the user credentials
-        if verify_user_credentials(username, password):
+        if database_operation.verify_user_credentials(username, password):
             # Redirect to the main page or any other page on successful login
             return redirect(url_for('get_main'))
         else:
@@ -75,8 +76,38 @@ def get_faciltiies():
 def get_main():
     return render_template("main.html")
 
-@app.route(r'/login.html')
+@app.route(r'/login.html', methods=['GET', 'POST'])
 def get_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        action = request.form['button']
+        if action == "register":
+            # Query the database to verify the user credentials
+            if database_operation.neurelo_add_user(username, password):
+                # Redirect to the main page or any other page on successful login
+                return redirect(url_for('get_main'))
+            else:
+                # If the credentials are not valid, render the login form again with an error message
+                error_message = "Invalid username or password."
+                print("We lost")
+                return render_template('login.html', error_message=error_message)
+            
+
+        elif action == "login":
+            if database_operation.neurelo_verify_user(username, password):
+                # Redirect to the main page or any other page on successful login
+                return redirect(url_for('get_main'))
+            else:
+                # If the credentials are not valid, render the login form again with an error message
+                error_message = "Invalid username or password."
+                print("We lost")
+                return render_template('login.html', error_message=error_message)
+    else:
+        # Render the login form template for GET requests
+        return render_template('login.html')
+    
+
     return render_template("login.html")
 
 @app.route(r'/about.html')
