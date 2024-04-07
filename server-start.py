01f9a82d24bf4e1999b2f51315e6ffab6e98f1f9
bruhitsyandrea <lisachen20000226@gmail.com>
2024-04-06 20:08:48 -0700

    login with database and error msg

--- server-start.py
+++ server-start.py
@@ -1,8 +1,10 @@
 import http.server
 import socketserver
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
+from database_operation import add_users_to_db
+from database_operation import verify_user_credentials
 
 # Define the port you want the server to listen on
 PORT = 8000
@@ -24,6 +26,40 @@ import os
     #httpd.serve_forever()
 
 app = Flask("parkSF")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Call the function to add users to the database
+        add_users_to_db(username, password)
+
+        # Redirect to a success page or render a success template
+        return render_template('register.html', success_message="Registration successful!")
+    else:
+        # Render the registration form template for GET requests
+        return render_template('register.html')
+
+
+@app.route('/login2', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Query the database to verify the user credentials
+        if verify_user_credentials(username, password):
+            # Redirect to the main page or any other page on successful login
+            return redirect(url_for('get_main'))
+        else:
+            # If the credentials are not valid, render the login form again with an error message
+            error_message = "Invalid username or password."
+            return render_template('login2.html', error_message=error_message)
+    else:
+        # Render the login form template for GET requests
+        return render_template('login2.html')
 
 @app.route(r'/users')
 def get_faciltiies():
